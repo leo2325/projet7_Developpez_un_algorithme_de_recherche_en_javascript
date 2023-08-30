db780f14639df78fd458d5047ec0cddaba16370c
leo23225 <leo_94310@hotmail.com>
2023-08-30 13:32:44 +0200

    LE 30/08

--- scripts/pages/searchBarSpecifics.js
+++ scripts/pages/searchBarSpecifics.js
@@ -1,4 +1,4 @@
-// DECLARATION DE CONSTANTES & VARIABLES
+// DECLARATION DES ELEMENTS DU DOM
 // elements de liste contenant les différentes options 
 const ingredientsSearchBar = document.getElementById('ingredientsSearchBar');
 const appareilsSearchBar = document.getElementById('appareilsSearchBar');
@@ -59,3 +59,144 @@ function launchSearchSpecific(event) {
 ingredientsListDownIcon.addEventListener('click', launchSearchSpecific);
 appareilsListDownIcon.addEventListener("click", launchSearchSpecific);
 ustensilesListDownIcon.addEventListener("click", launchSearchSpecific);
+
+
+
+
+
+
+
+
+// INPUT DE RECHERCHE SECONDAIRE 
+
+// DECLARATION DES ELEMENTS DU DOM
+// Les inputs
+const inputIngredientsSearch = document.getElementById("inputIngredientsSearch");
+const inputAppareilsSearch = document.getElementById("inputAppareilsSearch");
+const inputUstensilesSearch = document.getElementById("inputUstensilesSearch");
+
+
+// RECHERCHE PAR INGREDIENTS
+// Event de l'input de saisie utilisateur recherche ingredients
+inputIngredientsSearch.addEventListener('input', function (event) {
+    const searchText = event.target.value.trim().toLowerCase();
+    // Vider la liste de recherche ingrédients
+    ingredientsSearchListElement_box.innerHTML = '';
+
+    // Filtrer les recettes en fonction du texte saisi
+    const filteredRecipes = window.recipes.filter(function (recipe) {
+        return (
+            recipe.ingredients.some(function (ingredient) {
+                const ingredientName = ingredient.ingredient.toLowerCase();
+                return ingredientName.includes(searchText);
+            })
+        );
+    });
+
+    // Utilise la méthode reduce pour générer le contenu HTML des recettes filtrées
+    const filteredRecipesHTML = filteredRecipes.reduce(function (html, recipe) {
+        return html + recettesFactory(recipe).getRecettesCardDOM().outerHTML;
+    }, '');
+    // Met à jour l'affichage des recettes filtrées sur la page
+    recettesSection.innerHTML = filteredRecipesHTML;
+
+    // Filtrer les ingrédients en fonction du texte saisi
+    const filteredIngredients = window.recipes.reduce(function (ingredients, recipe) {
+        recipe.ingredients.forEach(function (ingredient) {
+            const ingredientName = ingredient.ingredient.toLowerCase();
+            if (ingredientName.includes(searchText)) {
+                ingredients.add(ingredientName);
+            }
+        });
+        return ingredients;
+    }, new Set());
+    // Mettre à jour la liste des ingrédients affichés dans la page
+    filteredIngredients.forEach(function (ingredientName) {
+        const ingredientElement = document.createElement('li');
+        ingredientElement.textContent = ingredientName;
+        ingredientElement.dataset.ingredient = ingredientName;
+        ingredientsSearchListElement_box.appendChild(ingredientElement);
+    });
+    // Mettre à jour le nombre total de recettes affichées dans l'élément prévu à cet effet
+    updateRecipeCount();
+});
+
+
+
+// RECHERCHE PAR APPAREILS
+// Event de l'input de saisie utilisateur recherche appareils
+inputAppareilsSearch.addEventListener('input', function (event) {
+    const searchText = event.target.value.trim().toLowerCase();
+    // Vider la liste de recherche appareils
+    appareilsSearchListElement_box.innerHTML = '';
+
+    // Filtrer les recettes en fonction du texte saisi
+    const filteredRecipes = window.recipes.filter(function (recipe) {
+        return (
+            recipe.appliance.toLowerCase().includes(searchText)
+        );
+    });
+
+    // Utilise la méthode reduce pour générer le contenu HTML des recettes filtrées
+    const filteredRecipesHTML = filteredRecipes.reduce(function (html, recipe) {
+        return html + recettesFactory(recipe).getRecettesCardDOM().outerHTML;
+    }, '');
+    recettesSection.innerHTML = filteredRecipesHTML;
+
+    const filteredAppareils = window.recipes.reduce(function (appareils, recipe) {
+        const appareilName = recipe.appliance.toLowerCase();
+        if (appareilName.includes(searchText)) {
+            appareils.add(appareilName);
+        }
+        return appareils;
+    }, new Set());
+
+    filteredAppareils.forEach(function (appareilName) {
+        const appareilElement = document.createElement('li');
+        appareilElement.textContent = appareilName;
+        appareilElement.dataset.appliance = appareilName;
+        appareilsSearchListElement_box.appendChild(appareilElement);
+    });
+    updateRecipeCount();
+});
+
+
+
+// RECHERCHE PAR USTENSILES
+inputUstensilesSearch.addEventListener('input', function (event) {
+    const searchText = event.target.value.trim().toLowerCase();
+    ustensilesSearchListElement_box.innerHTML = '';
+
+    const filteredRecipes = window.recipes.filter(function (recipe) {
+        return (
+            recipe.ustensils.some(function (ustensil) {
+                const ustensilName = ustensil.toLowerCase();
+                return ustensilName.includes(searchText);
+            })
+        );
+    });
+
+    const filteredRecipesHTML = filteredRecipes.reduce(function (html, recipe) {
+        return html + recettesFactory(recipe).getRecettesCardDOM().outerHTML;
+    }, '');
+    recettesSection.innerHTML = filteredRecipesHTML;
+
+    // Filtrer les ustensiles en fonction du texte saisi
+    const filteredUstensiles = window.recipes.reduce(function (ustensiles, recipe) {
+        recipe.ustensils.forEach(function (ustensil) {
+            const ustensilName = ustensil.toLowerCase();
+            if (ustensilName.includes(searchText)) {
+                ustensiles.add(ustensilName);
+            }
+        });
+        return ustensiles;
+    }, new Set());
+
+    filteredUstensiles.forEach(function (ustensilName) {
+        const ustensilElement = document.createElement('li');
+        ustensilElement.textContent = ustensilName;
+        ustensilElement.dataset.ustensil = ustensilName;
+        ustensilesSearchListElement_box.appendChild(ustensilElement);
+    });
+    updateRecipeCount();
+});
